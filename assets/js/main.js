598e2ae0935222e8a95167d16eda8021bf23b7ae
Danil|DGH <117117264+DGioriginal@users.noreply.github.com>
2022-12-10 19:03:33 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/*
-	Visualize by TEMPLATED
-	templated.co @templatedco
-	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
-*/
-
 $(function() {
 
 	// Vars.
@@ -11,7 +5,6 @@ $(function() {
 			$body = $('body'),
 			$wrapper = $('#wrapper');
 
-	// Breakpoints.
 		skel.breakpoints({
 			xlarge:	'(max-width: 1680px)',
 			large:	'(max-width: 1280px)',
@@ -19,15 +12,11 @@ $(function() {
 			small:	'(max-width: 736px)',
 			xsmall:	'(max-width: 480px)'
 		});
-
-	// Disable animations/transitions until everything's loaded.
 		$body.addClass('is-loading');
 
 		$window.on('load', function() {
 			$body.removeClass('is-loading');
 		});
-
-	// Poptrox.
 		$window.on('load', function() {
 
 			$('.thumbnails').poptrox({
